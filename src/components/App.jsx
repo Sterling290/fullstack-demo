a0fd9b70c555f973ea63e3a423f01dffd8b5710c
Sterling290 <sterling.becvar@gmail.com>
2020-01-16 16:43:42 -0700

    modal is working,retrive data from modal isnt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import Nav from './Nav.jsx';
 import BugTile from './BugTile.jsx';
+import Modl from './Modl.jsx';
 import exampleData from '../example-data/exampleData';
-
 import '../styles/App.scss';
+//___style______________________________________________________
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+//_________________________________________________________
 
 class App extends React.Component {
   constructor() {
@@ -11,11 +25,30 @@ class App extends React.Component {
     this.state = {
       filter: 'None',
       bugs: exampleData,
-      everything:exampleData
+      everything:exampleData,
+      modalIsOpen: false
     };
-    this.filterHandler = this.filterHandler.bind(this);
-  }
 
+    this.filterHandler = this.filterHandler.bind(this);
+  //__binding current with changes_____________________
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+//_________Methods_____________________________________
+openModal() {
+  this.setState({modalIsOpen: true});
+}
+
+afterOpenModal() {
+  // references are now sync'd and can be accessed.
+  this.subtitle.style.color = '#f00';
+}
+
+closeModal() {
+  this.setState({modalIsOpen: false});
+}
+//_________________________________________________________
   filterHandler(filter) {
     console.log(filter)
     if(filter !== "None"){
@@ -27,8 +60,6 @@ class App extends React.Component {
     }else if(filter === "None"){
       this.setState({bugs:this.state.everything})
     }
-    
-    //.then((something) =>{console.log(something)})
   }
   componentDidMount(){
      fetch("http://localhost:3000/api/data")
@@ -41,6 +72,50 @@ class App extends React.Component {
 
   render() {
     return (
+      <div>
+         {/* ____this is hard coded modal_____________________________________ */}
+        <button onClick={this.openModal}>Open Modal</button>
+          <div>
+        
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+ 
+            <h2 ref={subtitle => this.subtitle = subtitle}>add user</h2>
+            <button onClick={this.closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+            bugName: <input id ="one" /> <br />
+            bugDescription: <input id ="two"/><br />
+            reportedBy:<input id ="three" /><br />
+            createdDate:<input id ="four"/><br />
+            assignedTo: <input id ="five"/><br />
+            threatLevel: <input id ="six"/><br/>
+              <button onClick={() => {
+                
+                console.log("fun")
+                // app.post("/api/data", (req, res) => {
+                //   var obj =  {
+                //     "bugName": document.getElementById("one"),
+                //     "bugDescription": document.getElementById("two"),
+                //     "reportedBy": document.getElementById("three"),
+                //     "createdDate": document.getElementById("four"),
+                //     "assignedTo":document.getElementById("five"),
+                //     "threatLevel": document.getElementById("six")
+                //   }
+                //   //queries.create(obj)
+                //     .then(res.send(obj))
+                // })
+              }}>submit</button>
+         
+          </form>
+        </Modal>
+      </div>
+        {/* ___________________________________________________ */}
       <table>
         <Nav
           filterHandler={this.filterHandler}
@@ -57,8 +132,9 @@ class App extends React.Component {
           />
         ))}
       </table>
+      </div>
     );
   }
 }
-
+//ReactDOM.render(<App />, appElement)
 export default App;
---
+++ src/components/Modl.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// const Modl = (props) => (
+//     <div>
+//     <div id="myModal" class="modal"></div>
+//     <div class="modal-content">
+//     <span class="close">&times;</span>
+//     <p>Some text in the Modal..</p>
+//   </div>
+//   </div>
+// );
+
+// export default Modl;
